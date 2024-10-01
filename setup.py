6c70d2c764855dd785d6b7d8b44536ff7f00dd14
Shudipto Trafder <shudiptotrafder@gmail.com>
2024-10-01 14:55:18 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
-import setuptools
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="snowflakeid",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author="Shudipto",
+    description="Snowflake ID generation for asyncio",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/10XScale-in/snowflakeid",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
